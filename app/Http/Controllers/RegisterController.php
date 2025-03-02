9a6b325a9c0950f24cddbe83629200d939cfb078
rvaldez20 <rvaldez20@gmail.com>
2025-03-02 12:41:58 -0600

    [ADD] creamos el primer user, se grega el username an el modelo(fillable)

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -24,7 +25,14 @@ class RegisterController extends Controller
          'password' => 'required|confirmed|min:6'
       ]);
 
-      dd('Creando Usuario');
+    //   dd('Creando Usuario');
+
+      User::create([
+        'name' => $request->name,
+        'username' => $request->username,
+        'email' => $request->email,
+        'password' => $request->password
+      ]);
 
 
 
