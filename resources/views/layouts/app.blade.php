401de503a12cc36abcd6546d68730142d3f97800
rvaldez20 <rvaldez20@gmail.com>
2025-03-27 21:41:21 -0600

    [ADD] refactory a la forma de cerrar sesion con un form y route post

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,9 +20,12 @@
                         Hola: <span class="font-normal">{{ auth()->user()->username }}</span>
                      </a>
 
-                     <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('logout') }}">
-                        Cerrar Sesión
-                     </a>
+                     <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="font-bold uppercase text-gray-600 text-sm">
+                           Cerrar Sesión
+                        </button>
+                     </form>
                   </nav>
                @endauth
 
