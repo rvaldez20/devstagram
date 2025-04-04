dfae0e0c7b44200a85d668994d63c62c2f44ce51
rvaldez20 <rvaldez20@gmail.com>
2025-04-03 20:09:34 -0600

    [ADD] pasmos desde el controller el user a la vista dasboard: el username para la url

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,8 @@ class PostController extends Controller
     public function index(User $user)
     {
         // dd($user->username);
-        return view('dashboard');
+        return view('dashboard', [
+            'user' => $user
+        ]);
     }
 }
