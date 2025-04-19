924e12e05bca3b57657e528480436ada5de352ea
rvaldez20 <rvaldez20@gmail.com>
2025-04-19 14:20:34 -0600

    [ADD] Se implenta el subir las imagenes al servidor

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Laravel\Facades\Image;
+
+
 
 class ImagenController extends Controller
 {
     public function store(Request $request)
     {
         $imagen = $request->file('file');
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
 
-        return response()->json(['imagen' => $imagen->extension()]);
+        $imagenServidor = Image::read($imagen)->resize(1000,1000);
+
+        // $imagenServidor = Image::read($imagen)->resize(1000,1000, function($constrain){
+        //     $constrain->aspectRatio();
+        //     $constrain->upsize();
+        // });
+
+        $imagePath = public_path('uploads') . "/" . $nombreImagen;
+        $imagenServidor->save($imagePath);
+
+        return response()->json(['imagen' => $nombreImagen]);
     }
 }
