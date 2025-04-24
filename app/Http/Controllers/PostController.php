f0a3f5ec9508eeea3135c37b2c9890e72efb00f5
rvaldez20 <rvaldez20@gmail.com>
2025-04-23 21:07:44 -0600

    [ADD] otra froma de guardar nuevos registros

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,12 +34,19 @@ class PostController extends Controller
             'imagen' => 'required'
         ]);
 
-        Post::create([
-            'titulo' => $request->titulo,
-            'descripcion' => $request->descripcion,
-            'imagen' => $request->imagen,
-            'user_id' => auth()->user()->id
-        ]);
+        // Post::create([
+        //     'titulo' => $request->titulo,
+        //     'descripcion' => $request->descripcion,
+        //     'imagen' => $request->imagen,
+        //     'user_id' => auth()->user()->id
+        // ]);
+
+        $post = new Post;
+        $post->titulo = $request->titulo;
+        $post->descripcion = $request->descripcion;
+        $post->imagen = $request->imagen;
+        $post->user_id = auth()->user()->id;
+        $post->save();
 
         return redirect()->route('posts.index', auth()->user()->username);
     }
