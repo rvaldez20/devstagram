6a30dfe3cf6b027bd7cfc2e36179d30eae304fcc
rvaldez20 <rvaldez20@gmail.com>
2025-04-23 21:33:26 -0600

    [ADD] creamos la relacion has many, one user-many posts

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        // One to many : un usuario puede tener muchas publicaciones
+        return $this->hasMany(Post::class, 'user_id');
+    }
 }
